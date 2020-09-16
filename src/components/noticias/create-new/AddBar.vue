7f88a3b3762aac3751457d0fe1b0fa11b5cf4647
ValenDesdel <vdestefano@correo.unimet.edu.ve>
2020-09-16 14:55:40 -0400

    Lista vista HN7

---
+++ src/components/noticias/create-new/AddBar.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="add-introduction-new-content my-shadow">
+  <div class="dropdown">
+    <button class="drop-btn"><a>°°°</a></button>
+    <div class="drop-content">
+        <a>Agregar título H2</a>
+        <a>Agregar título H3</a>
+        <a>Agregar título H4</a>
+        <a>Agregar Texto</a>
+        <a>Agregar Imagen</a>
+        <a>Eliminar</a>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+/**
+ * TODO listas por agg en AddIntroductionNew
+ * !ajustar tamaño de textarea al contenido
+ */
+
+export default {
+    name: 'AddIntroductionNew',
+    data() {
+
+    },
+    components: {
+    },
+}
+</script>
--- src/views/news/Create.vue
+++ src/views/news/Create.vue
@@ -4,6 +4,7 @@
     <Article topNav="top-two" />
     <AddHeadNew />
     <AddIntroductionNew />
+    <AddBar />
     <ConfigList />
 </div>
 </template>
@@ -13,6 +14,7 @@ import Article from "../../components/noticias/Article";
 import SubNavBar from '../../components/noticias/SubNavBar';
 import AddHeadNew from '../../components/noticias/create-new/AddHeadNew';
 import AddIntroductionNew from '../../components/noticias/create-new/AddIntroductionNew';
+import AddBar from '../../components/noticias/create-new/AddBar';
 import ConfigList from '../../components/noticias/create-new/ConfigList';
 export default {
     name: 'Create',
@@ -21,6 +23,7 @@ export default {
         SubNavBar,
         AddHeadNew,
         AddIntroductionNew,
+        AddBar,
         ConfigList
     },
     data() {
