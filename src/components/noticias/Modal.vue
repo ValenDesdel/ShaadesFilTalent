37ad8d52c25f4072b0bb5411a4470f78ae57c24d
ValenDesdel <vdestefano@correo.unimet.edu.ve>
2020-09-23 17:29:53 -0400

    Modal listo y correccion de colores

--- src/components/noticias/Modal.vue
+++ src/components/noticias/Modal.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="container-fluid user-follow-content-2" v-bind:class="{active: btnSearchHeader}">
     <div class="row user-follow-list-2" v-bind:class="{active: btnSearchHeader}">
-        <div class="add-introduction-new-content my-shadow">
+        <div class="extracto-modal-div">
+            <div class="title-modal">
+                <a>EXTRACTO</a>
+            </div>
             <textarea class="extracto-modal" name="extracto" id="extractoNew" cols="auto" placeholder="Escribe tu extracto aquí"></textarea>
+            <div class="btn-extracto-modal">
+                <a class="a-extracto-modal-save">Guardar</a>
+                <a class="a-extracto-modal-close">Cerrar</a>
+            </div>
         </div>
     </div>
 </div>
